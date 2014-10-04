6dac5d1df8943648d82a6408762cb326f92ae055
Corey Haines <coreyhaines@gmail.com>
2014-10-04 17:33:41 -0500

    Can look at Head and Tail of lists

--- list.rb
+++ list.rb
@@ -1,2 +1,17 @@
-EmptyList = Cons[True][Noop]
-IsEmpty = Car
+EmptyList = Cons[Cons[True][Noop]][Cons[Cons[True][Noop]][Noop]]
+IsEmpty =
+-> list {
+  Car[Car[list]]
+}
+Unshift =
+-> element { -> list {
+  Cons[Cons[False][element]][list]
+}}
+Head =
+-> list {
+  Cdr[Car[list]]
+}
+Tail =
+-> list {
+  Cdr[list]
+}
--- list_examples.rb
+++ list_examples.rb
@@ -1,10 +1,23 @@
 load './core.rb'
 load './list.rb'
+load './numbers.rb'
 def title(msg)
   puts "------#{msg}-----"
 end
 
 title "Lists"
 
-title "Empty List"
+title "Empty List (2)"
 Assert[IsEmpty[EmptyList]]
+Refute[IsEmpty[Unshift[$one][EmptyList]]]
+
+title "Head (3)"
+Assert[NumbersEqual[$one][Head[Unshift[$one][EmptyList]]]]
+Refute[NumbersEqual[$two][Head[Unshift[$one][EmptyList]]]]
+Assert[NumbersEqual[$two][Head[Unshift[$two][Unshift[$one][EmptyList]]]]]
+
+title "Tail (1)"
+Assert[IsEmpty[Tail[Unshift[$one][EmptyList]]]]
+
+title "Tail of EmptyList should be EmptyList (1)"
+Assert[IsEmpty[Tail[EmptyList]]]
