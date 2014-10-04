e0456516fa0c0451cfab79861954c166ad685772
Corey Haines <coreyhaines@gmail.com>
2014-10-04 17:43:34 -0500

    Add example count for each title, so I can make sure they are all executed

--- core_examples.rb
+++ core_examples.rb
@@ -3,34 +3,34 @@ def title(msg)
   puts "------#{msg}-----"
 end
 
-title "Tuples"
+title "Tuples (2)"
 tuple = Cons[1][2]
-puts Car[tuple]
-puts Cdr[tuple]
+puts (Car[tuple] == 1 ? "T" : "F")
+puts (Cdr[tuple] == 2 ? "T" : "F")
 
 title "Logic"
 
-title "If True"
+title "If True (1)"
 If[PrintTrue][PrintFalse][True]
-title "If False"
+title "If False (1)"
 If[PrintTrue][PrintFalse][False]
 
-title "Assert/Refute"
+title "Assert/Refute (2)"
 Assert[True]
 Refute[False]
 
-title "And"
+title "And (4)"
 Assert[And[True][True]]
 Refute[And[False][False]]
 Refute[And[True][False]]
 Refute[And[False][True]]
 
-title "Or"
+title "Or (4)"
 Assert[Or[True][True]]
 Refute[Or[False][False]]
 Assert[Or[True][False]]
 Assert[Or[False][True]]
 
-title "Not"
+title "Not (2)"
 Assert[Not[False]]
 Refute[Not[True]]
--- numbers_examples.rb
+++ numbers_examples.rb
@@ -5,7 +5,7 @@ def title(msg)
 end
 title "Numbers"
 
-title "Checking if things are zero"
+title "Checking if things are zero (8)"
 Assert[IsZero[$zero]]
 Refute[IsZero[Succ[$zero]]]
 Assert[IsZero[Pred[Succ[$zero]]]]
@@ -15,7 +15,7 @@ Assert[IsZero[Pred[$one]]]
 Refute[IsZero[Pred[$two]]]
 Assert[IsZero[Pred[Pred[$two]]]]
 
-title "Checking if two numbers are equal"
+title "Checking if two numbers are equal (7)"
 Assert[NumbersEqual[$zero][$zero]]
 Refute[NumbersEqual[$zero][$one]]
 Refute[NumbersEqual[$one][$zero]]
@@ -24,7 +24,7 @@ Assert[NumbersEqual[$two][$two]]
 Refute[NumbersEqual[$two][$three]]
 Refute[NumbersEqual[$three][$two]]
 
-title "Addition"
+title "Addition (8)"
 Assert[IsZero[NumbersAdd[$zero][$zero]]]
 Assert[NumbersEqual[$one][NumbersAdd[$one][$zero]]]
 Assert[NumbersEqual[$two][NumbersAdd[$two][$zero]]]
@@ -34,7 +34,7 @@ Assert[NumbersEqual[$two][NumbersAdd[$one][$one]]]
 Assert[NumbersEqual[$three][NumbersAdd[$two][$one]]]
 Assert[NumbersEqual[$three][NumbersAdd[$one][$two]]]
 
-title "Subtraction"
+title "Subtraction (7)"
 Assert[IsZero[NumbersSubtract[$zero][$zero]]]
 Assert[IsZero[NumbersSubtract[$one][$one]]]
 Assert[NumbersEqual[$one][NumbersSubtract[$one][$zero]]]
