54c502fa40fb4e91d0a1107bc3c660c80d5fb04a
Corey Haines <coreyhaines@gmail.com>
2014-10-05 09:00:15 -0500

    Put some negative examples in for Assert/Refute

--- testing_examples.rb
+++ testing_examples.rb
@@ -1,8 +1,22 @@
 require_relative 'testing.rb'
 require_relative 'numbers.rb'
-title "Assert/Refute (2)"
+title "Assert/Refute (4)"
 Assert[True]
+begin
+  Assert[False]
+  Print["Asserting False failed"].(Noop)
+rescue
+  Print["T"].(Noop)
+end
+
 Refute[False]
+begin
+  Refute[True]
+  Print["Refuting True failed"].(Noop)
+rescue
+  Print["T"].(Noop)
+end
+
 
 title "AssertEqual"
 
